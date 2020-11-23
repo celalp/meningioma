c44bb2a1ab9ec8a9f9de2e956591df84875a5cb3
alper <celikalper84@gmail.com>
2020-11-23 16:45:34 -0500

    simplified analysis pipeline, the log file is generated within R and no need to direct stdout/stderr

--- analysis/watcher.sh
+++ analysis/watcher.sh
@@ -1,24 +1,9 @@
 #! /bin/bash
 
-DB=$1
-PARAMS=$2
-
-echo $DB
-echo $PARAMS
+PARAMS=$1
 
 mkdir -p analysis_logs
+cd analysis
 
-while true
-do
-	samples=$(psql -A -t -d $DB -w -c "select sampleid from samples_users.samples where status='queued for analysis'")
-	if [ ${#samples} -eq 0 ]
-	then
-		sleep 10
-	else
-		for sample in $samples
-		do
-			Rscript analysis/analysis_pipeline.R -s $sample -y $PARAMS &>> analysis_logs/$sample.log
-		done
-	fi
-done
+Rscript analysis_pipeline.R -y $PARAMS 
 
